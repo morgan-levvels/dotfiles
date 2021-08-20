24ae3c4320ea623c3b72faaae6087ba522be53cc
morgan <morgan@dunamu.com>
2021-08-20 13:25:06 +0900

    added config.fish for local

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,8 @@
-	set -Ux JAVA_HOME (/usr/libexec/java_home -v 11)
-	direnv hook fish | source
+set -Ux JAVA_HOME (/usr/libexec/java_home -v 11)
+direnv hook fish | source
 
 fish_add_path /usr/local/sbin
+
+set local_config ~/.config/fish/config.local.fish
+echo $local_config
+test -r $local_config; and source $local_config

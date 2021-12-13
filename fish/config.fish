d84f5ade260738b88f126ebdfbedc0b57a101c0f
morgan <morgan@dunamu.com>
2021-12-13 14:29:42 +0900

    fixed local config name

--- fish/config.fish
+++ fish/config.fish
@@ -3,5 +3,5 @@ direnv hook fish | source
 
 fish_add_path /usr/local/sbin
 
-set local_config ~/.config/fish/config.local.fish
+set local_config ~/.config/fish/local.config.fish
 test -r $local_config; and source $local_config
